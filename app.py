943658fefec9077d8dcd28098703840309ab5fd5
David Xu <dxaviud@gmail.com>
2021-03-07 00:03:59 -0500

    finished

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:IShallOvercome@localhost/lexus'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:IShallOvercome@localhost/bugtracker'
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qlodaemmvyawth:d7d8741f260da924ff32c455b6975b3e401e783fece05ffebc2ff1b3c86becf2@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d1evkrmjak1kmi'
@@ -17,19 +17,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
+class Bug(db.Model):
+    __tablename__ = 'bug'
     id = db.Column(db.Integer, primary_key=True)
-    developername = db.Column(db.String(200))
-    bugtype = db.Column(db.String(200))
-    bugpriority = db.Column(db.String(200))
-    bugsummary = db.Column(db.Text())
+    name = db.Column(db.String(200))
+    type = db.Column(db.String(200))
+    priority = db.Column(db.String(200))
+    summary = db.Column(db.Text())
 
-    def __init__(self, developername, bugtype, bugpriority, bugsummary):
-        self.developername = developername
-        self.bugtype = bugtype
-        self.bugpriority = bugpriority
-        self.bugsummary = bugsummary
+    def __init__(self, name, type, priority, summary):
+        self.name = name
+        self.type = type
+        self.priority = priority
+        self.summary = summary
 
 
 @app.route('/')
@@ -46,8 +46,8 @@ def submit():
         bugsummary = request.form['bugsummary']
         if developername == '' or bugtype == '':
             return render_template('index.html', message='Please enter required fields.')
-        if db.session.query(Feedback).filter(Feedback.developername == developername).count() == 0:
-            data = Feedback(developername, bugtype, bugpriority, bugsummary)
+        if db.session.query(Bug).filter(Bug.name == developername).count() == 0:
+            data = Bug(developername, bugtype, bugpriority, bugsummary)
             db.session.add(data)
             db.session.commit()
             send_mail(developername, bugtype, bugpriority, bugsummary)
